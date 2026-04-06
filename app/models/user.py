fc3e5d998792243ecfb8ed6a0c67d66a9aaf42f7
Avneesh Kumar <65752440+Avneesh12@users.noreply.github.com>
2026-04-06 14:39:47 +0000

    users table name change

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,7 @@ from app.core.database import Base
 
 
 class User(Base):
-    __tablename__ = "users_twin"
+    __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
